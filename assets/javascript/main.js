f4489dadf92e4d5335f64f6da4b39b60d9a7ee4b
Nicholas Lopes Leite <nicklleite@gmail.com>
2014-10-20 15:39:44 -0200

    Update main.js
    Ocultando o menu ao clicar em um item (mobile).

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,6 +14,7 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        $(".navbar-collapse").removeClass("in");
         event.preventDefault();
     });
 });
