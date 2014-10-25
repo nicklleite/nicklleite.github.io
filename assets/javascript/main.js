aa900e1dda82d4f88f67f3d70849556aab8fa80f
Nicholas Lopes Leite <nicklleite@gmail.com>
2014-10-25 00:02:57 -0200

    Atualizando conteúdo e charset

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,39 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 100) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+$(document).ready(function(){
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 100) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    $('.section').each(function(){
+        var $obj = $(this);
+         
+        $(window).scroll(function() {
+            var yPos = -( $(window).scrollTop() / $obj.data('speed') );
+            var bgpos = '50% '+ yPos + 'px';
+         
+            $obj.css('background-position', bgpos );
+        });
+    });
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        $(".navbar-collapse").removeClass("in");
         event.preventDefault();
     });
+
+    //Calculando minha idade atual.
+    var dt_nascimento = new Date(1991,10,31);
+    var hoje = new Date();        
+
+    var dayDiff = Math.ceil(hoje.getTime() - dt_nascimento.getTime()) / (1000 * 60 * 60 * 24 * 365);
+    var idade = parseInt(dayDiff);
+    $('.idade-atual').html(idade);
+
 });
